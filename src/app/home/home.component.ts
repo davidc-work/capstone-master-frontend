43a073ab724567be09531a444e6261c6dd429a73
davidc-work <dceleste.work@gmail.com>
2022-01-24 17:44:21 -0500

    added slideshow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var mainGallery = tns({
+      container: '.main-gallery',
+      items: 1,
+      slideBy: 'page',
+      autoplay: true,
+      mouseDrag: true
+    });
   }
 
 }
