c1229f17cef0e4bef6de862f3b8bff9b1d40348a
davidc-work <dceleste.work@gmail.com>
2022-02-02 19:06:33 -0500

    redesigned mutual fund page

--- src/app/fund/fund.component.ts
+++ src/app/fund/fund.component.ts
@@ -6,6 +6,9 @@ import { Fund } from './fund.model';
 import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { TransactionComponent } from '../transaction/transaction.component';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   providers: [TransactionComponent],
@@ -17,6 +20,7 @@ export class FundComponent implements OnInit {
   fund:Fund = {};
   keys:any;
   id:number = 0;
+  chart: any;
   
   @ViewChild(TransactionComponent) transactionComponent!: TransactionComponent;
   
@@ -37,16 +41,60 @@ export class FundComponent implements OnInit {
       this.id = +params['id'];
       this.fundService.getFund(this.id).subscribe(payload=> {
         this.fund = payload;
+
+        this.fund.priceChange = this.fund.priceChange?.replace('$', '');
+        
+        const priceChange = +this.fund.priceChange!
+        const price = +(this.fund.price!).replace('$', '');
+        const v = 100 * (priceChange / (price - priceChange));
+        this.fund.positiveChange = v > 0;
+        this.fund.priceChangePercent = Math.abs(v).toFixed(2) + '%';
+
         this.titleService.setTitle('RVProtect - ' + this.fund.name);
         this.keys = Object.keys(this.fund);
+        
+        const chartElement: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('performance-chart');
+        const ctx = chartElement.getContext('2d');
+        this.chart = new Chart(chartElement, {
+          type: 'bar',
+          data: {
+            labels: ['One Year', 'Five Year', 'Ten Year', 'Since Inception'],
+            datasets: [{
+              label: '% change',
+              data: [
+                this.fund.oneyr?.slice(0, -1),
+                this.fund.fiveyr?.slice(0, -1),
+                this.fund.tenyr?.slice(0, -1),
+                this.fund.sinceInception?.slice(0, -1)
+              ],
+              backgroundColor: ['#ff0000', '#ff4800', '#ff6f00', '#ffa600']
+            }]
+          }, options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true
+              },
+            }
+          }
+        });
       });
     });
   }
 
+  commas(n: any) {
+    return (+n).toLocaleString();
+  }
+
+  viewStock(id: number) {
+    this.router.navigateByUrl('/stocks/' + id + '?return=funds');
+  }
+
   back() {
-    var e: HTMLElement = <HTMLElement>document.getElementsByClassName('scroll')[0];
-    e.style.animation = '0.25s out-to-right';
-    setTimeout(() => this.router.navigateByUrl('/funds'), 250);
+    var e: HTMLElement = <HTMLElement>document.getElementsByClassName('fund-main')[0];
+    e.className += ' hide';
+    setTimeout(() => this.router.navigateByUrl('/funds'), 500);
   }
 
   viewStocks() {
